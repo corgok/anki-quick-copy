9daae6fb3217ed6ef8f4e1d832fa6499e076f465
corgok <44769412+corgok@users.noreply.github.com>
2026-03-24 20:55:11 +0100

    Migrate from PyQt5 to PyQt6 and improve clipboard logic
    Updated import from PyQt5 to PyQt6 and modified note type retrieval. Adjusted clipboard handling for field data.

--- __init__.py
+++ __init__.py
@@ -1,23 +1,20 @@
 # import the main window object (mw) from aqt
+# this should work with Version ⁨25.02.7 (1b882285)⁩
 import aqt
 import aqt.editor
 import aqt.gui_hooks
-from PyQt5.QtWidgets import QApplication
-# import sys
-
-# using this hook, copy the field we're interested in into the clipboard editor_did_load_note
+from PyQt6.QtWidgets import QApplication
 
 def editor_loaded_note(editor: aqt.editor.Editor):
-    # get addon configuration
     config = aqt.mw.addonManager.getConfig(__name__)
-    # check the note type
-    note_type_name = editor.note.model()['name']
+    
+    note_type_name = editor.note.note_type()['name']
+    
     if note_type_name in config:
         field_name = config[note_type_name]
-        field_data = editor.note[field_name]
-        # copy to clipboard
-        clipboard = QApplication.clipboard()
-        clipboard.setText(field_data)
-
+        if field_name in editor.note:
+            field_data = editor.note[field_name]
+            clipboard = QApplication.clipboard()
+            clipboard.setText(field_data)
 
 aqt.gui_hooks.editor_did_load_note.append(editor_loaded_note)
